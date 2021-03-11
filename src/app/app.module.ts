df6518ad40ec974a7c392a485c78ebbdc47860c8
Tito-Rollis <ouvalres@gmail.com>
2021-03-11 07:26:08 +0700

    template created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PlaylistsComponent } from './playlists/playlists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistsComponent
   ],
   imports: [
     BrowserModule,
