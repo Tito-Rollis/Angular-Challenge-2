f411e3555a567b6ffb6a943d138d54650f3c5e78
Tito-Rollis <ouvalres@gmail.com>
2021-03-11 09:48:00 +0700

    Show all the starter playlist in the page

--- src/app/playlists/playlists.component.ts
+++ src/app/playlists/playlists.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { Playlist, Song, PlaylistComponent } from './playlist.model';
 
 @Component({
   selector: 'app-playlists',
   templateUrl: './playlists.component.html',
-  styleUrls: ['./playlists.component.css']
+  styleUrls: ['./playlists.component.css'],
 })
 export class PlaylistsComponent implements OnInit {
+  playlists: Playlist[] = [
+    {
+      name: 'Kopikustik',
+      totalDuration: 5,
+      totalSongs: 2,
+      description:
+        'More than a coffee, this is all of your favorite accoustic songs.',
+      songs: [
+        {
+          title: 'Cigarettes of ours',
+          artist: 'Ardhito Pramono',
+          duration: 3,
+        },
+        {
+          title: 'Walking Back Home',
+          artist: 'Vira Talisa',
+          duration: 2,
+        },
+      ],
+    },
+    {
+      name: 'Anime Hits',
+      totalDuration: 13,
+      totalSongs: 3,
+      description: 'Listen to your favorite Anime songs, all in one playlist.',
+      songs: [
+        {
+          title: 'Renai Circulation',
+          artist: 'Kana Hanazawa',
+          duration: 4,
+        },
+        {
+          title: 'Platinum Disco',
+          artist: 'Tsukihi Phoenix',
+          duration: 4,
+        },
+        {
+          title: 'Silhouette',
+          artist: 'KANA-BOON',
+          duration: 5,
+        },
+      ],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  log() {
+    console.log(this.playlists[0].songs);
   }
 
+  ngOnInit(): void {}
 }
